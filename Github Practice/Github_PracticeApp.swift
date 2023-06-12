0ba9ccea93050743646eb039f63a7501a87e8abc
Sabrina Naseri <sabrina.naseri13@gmail.com>
2023-06-12 11:22:42 -0400

    Initial Commit

---
+++ Github Practice/Github_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_PracticeApp.swift
+//  Github Practice
+//
+//  Created by Sabrina Naseri on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
